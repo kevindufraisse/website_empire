a75caa387bd3cead18640a7dc1c86e85c39a4bc9
kevin <kevindufraisse@gmail.com>
2026-01-22 14:43:31 +0000

    Fix Vidalytics video loading on thank-you page

--- app/thank-you/page.tsx
+++ app/thank-you/page.tsx
@@ -41,27 +41,40 @@ export default function DemoThankYouPage() {
 
   // Load Vidalytics script
   useEffect(() => {
-    const script = document.createElement('script')
-    script.type = 'text/javascript'
-    script.innerHTML = `
-      (function (v, i, d, a, l, y, t, c, s) {
-        y='_'+d.toLowerCase();c=d+'L';if(!v[d]){v[d]={};}if(!v[c]){v[c]={};}if(!v[y]){v[y]={};}var vl='Loader',vli=v[y][vl],vsl=v[c][vl + 'Script'],vlf=v[c][vl + 'Loaded'],ve='Embed';
-        if (!vsl){vsl=function(u,cb){
-          if(t){cb();return;}s=i.createElement("script");s.type="text/javascript";s.async=1;s.src=u;
-          if(s.readyState){s.onreadystatechange=function(){if(s.readyState==="loaded"||s.readyState=="complete"){s.onreadystatechange=null;vlf=1;cb();}};}else{s.onload=function(){vlf=1;cb();};}
-          i.getElementsByTagName("head")[0].appendChild(s);
-        };}
-        vsl(l+'loader.min.js',function(){if(!vli){var vlc=v[c][vl];vli=new vlc();}vli.loadScript(l+'player.min.js',function(){var vec=v[d][ve];t=new vec();t.run(a);});});
-      })(window, document, 'Vidalytics', '${currentVideo.embedId}', 'https://fast.vidalytics.com/embeds/ydq0TH7p/${currentVideo.videoId}/');
-    `
-    document.body.appendChild(script)
+    // Clean up previous Vidalytics instances
+    const oldContainerEn = document.getElementById(videoIds.en.embedId)
+    const oldContainerFr = document.getElementById(videoIds.fr.embedId)
+    if (oldContainerEn) oldContainerEn.innerHTML = ''
+    if (oldContainerFr) oldContainerFr.innerHTML = ''
+    
+    // Small delay to ensure DOM is ready
+    const timer = setTimeout(() => {
+      const script = document.createElement('script')
+      script.type = 'text/javascript'
+      script.id = `vidalytics-script-${currentVideo.videoId}`
+      script.innerHTML = `
+        (function (v, i, d, a, l, y, t, c, s) {
+          y='_'+d.toLowerCase();c=d+'L';if(!v[d]){v[d]={};}if(!v[c]){v[c]={};}if(!v[y]){v[y]={};}var vl='Loader',vli=v[y][vl],vsl=v[c][vl + 'Script'],vlf=v[c][vl + 'Loaded'],ve='Embed';
+          if (!vsl){vsl=function(u,cb){
+            if(t){cb();return;}s=i.createElement("script");s.type="text/javascript";s.async=1;s.src=u;
+            if(s.readyState){s.onreadystatechange=function(){if(s.readyState==="loaded"||s.readyState=="complete"){s.onreadystatechange=null;vlf=1;cb();}};}else{s.onload=function(){vlf=1;cb();};}
+            i.getElementsByTagName("head")[0].appendChild(s);
+          };}
+          vsl(l+'loader.min.js',function(){if(!vli){var vlc=v[c][vl];vli=new vlc();}vli.loadScript(l+'player.min.js',function(){var vec=v[d][ve];t=new vec();t.run(a);});});
+        })(window, document, 'Vidalytics', '${currentVideo.embedId}', 'https://fast.vidalytics.com/embeds/ydq0TH7p/${currentVideo.videoId}/');
+      `
+      document.body.appendChild(script)
+    }, 100)
 
     return () => {
-      if (script.parentNode) {
-        script.parentNode.removeChild(script)
+      clearTimeout(timer)
+      // Remove old scripts
+      const oldScript = document.getElementById(`vidalytics-script-${currentVideo.videoId}`)
+      if (oldScript && oldScript.parentNode) {
+        oldScript.parentNode.removeChild(oldScript)
       }
     }
-  }, [currentVideo.embedId, currentVideo.videoId])
+  }, [lang, currentVideo.embedId, currentVideo.videoId])
   
   return (
     <main className="relative min-h-screen bg-gradient-to-b from-black via-[#0f0f0f] to-black pt-24 md:pt-32">
